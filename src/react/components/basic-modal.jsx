a2b27c0355667c1ae566b73d2498712d5efa0b6a
Greg Nam <okfarm09@gmail.com>
2019-01-29 16:46:08 +0900

    Add modal component first commit

---
+++ src/react/components/basic-modal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { IoMdCheckmark, IoIosClose } from "react-icons/io";
+import ButtonAnchor from "./button-a";
+
+const BasicModal = (props) => {
+    const icon = props.icon === "good"? <IoMdCheckmark className={props.iconClass} /> : <IoIosClose className={props.iconClass} />;
+    const buttons = props.buttons.map(
+        item => <ButtonAnchor {...item} />
+    );
+    return (
+        <div className={props.modalClass} id={props.elemId}>
+            {icon}
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            {buttons}
+        </div>
+    );
+}
+
+BasicModal.propTypes = {
+    icon: PropTypes.string,
+    iconClass: PropTypes.string,
+    buttons: PropTypes.array,
+    modalClass: PropTypes.string,
+    elemId: PropTypes.string,
+}
+
+BasicModal.defaultProps = {
+    icon: "bad",
+    iconClass: "modal-icon",
+    buttons: [],
+    modalClass: "modal-popup",
+    elemId: "box"
+}
+
+export default BasicModal;
